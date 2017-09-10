54e77a7cb1313e1aada0b8743985f3846ec4c261
Jessica Schlauderaff <jessicaschlauderaff@gmail.com>
2017-09-10 19:35:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var words = []
-  for(i=0; i < musicians.length; i++) {
+  for(var i=0; i < musicians.length; i++) {
     words[i] = musicians[i] + " plays " + instruments[i]
   }
   return words
